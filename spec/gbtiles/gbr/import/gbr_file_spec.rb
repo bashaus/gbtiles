bced4ff7b0a2041165fc58189be28d092ef4eb7a
Bashkim Isai <me@bashkim.com>
2015-07-25 23:29:03 +0100

    created spec for producer

--- spec/gbtiles/gbr/import/gbr_file_spec.rb
+++ spec/gbtiles/gbr/import/gbr_file_spec.rb
@@ -3,7 +3,7 @@ require "gbtiles/gbr/import/gbr_file"
 RSpec.describe GBTiles::GBR::Import::GBRFile, "#open" do
   context "with an invalid file header" do
     before do
-      @file = File.open "spec/fixtures/gbr_file/invalid-header.gbr"
+      @file = File.open "spec/fixtures/gbr_file/invalid-header.gbr", "rb"
     end
 
     it "raises an error" do
@@ -13,11 +13,22 @@ RSpec.describe GBTiles::GBR::Import::GBRFile, "#open" do
 
   context "with a valid file header" do
     before do
-      @file = File.open "spec/fixtures/gbr_file/valid.gbr"
+      @file = File.open "spec/fixtures/gbr_file/valid.gbr", "rb"
     end
 
     it "doesn't raise an error" do
       expect { @gbr = GBTiles::GBR::Import::GBRFile.open @file }.not_to raise_error(IOError)
+    end
+
+    context "on file process" do
+      before do
+        @gbr = GBTiles::GBR::Import::GBRFile.open @file
+      end
+
+      it "has one producer" do
+        expect(@gbr.tile_set.producer.count).to eql 1
+        producer = @gbr.tile_set.producer.first
+      end
     end
   end
 end
--- spec/gbtiles/gbr/tile_set/objects/producer_spec.rb
+++ spec/gbtiles/gbr/tile_set/objects/producer_spec.rb
@@ -1,25 +1,16 @@
-# require "gbtiles/gbr/import/gbr_file"
+require "gbtiles/gbr/import/gbr_file"
 
-# RSpec.describe GBTiles::GBR::Import::GBRFile, "#open" do
-#   context "with an invalid file header" do
-#   end
+RSpec.describe GBTiles::GBR::TileSet::Objects::Producer, "#initFromBitString" do
+  before do
+    @file = File.open "spec/fixtures/producer/partial.gbr", "rb"
+  end
 
-#   context "with a valid file header" do
-#     before do
-#       @file = File.open "spec/fixtures/win.gbr"
-#     end
+  it "reads data" do
+    producer = GBTiles::GBR::TileSet::Objects::Producer.initFromBitString @file.read
 
-#     it "validates a file" do
-#       @gbr = GBTiles::GBR::Import::GBRFile.open @file
-
-#       expect(@gbr.tile_set.producer.count).to eql 1
-
-#       producer = @gbr.tile_set.producer.first
-
-#       expect(producer.object_type).to eql GBTiles::GBR::TileSet::OBJECT_TYPE[:producer]
-#       expect(producer.name).to eql "Gameboy Tile Designer"
-#       expect(producer.version).to eql "2.2"
-#       expect(producer.info).to eql "Home: www.casema.net/~hpmulder"
-#     end
-#   end
-# end
+    expect(producer.object_type).to eql GBTiles::GBR::TileSet::OBJECT_TYPE[:producer]
+    expect(producer.name).to eql "Gameboy Tile Designer"
+    expect(producer.version).to eql "2.2"
+    expect(producer.info).to eql "Home: www.casema.net/~hpmulder"
+  end
+end

0f2feb2a3bbd30424e5459d875538376c2d4529f
Bashkim Isai <me@bashkim.com>
2015-07-25 23:40:58 +0100

    using object_type instead of class name to filter objects in tile_set

--- lib/gbtiles/gbr/tile_set/tile_set.rb
+++ lib/gbtiles/gbr/tile_set/tile_set.rb
@@ -11,43 +11,43 @@ module GBTiles
 
         def producer
           @objects.select{ |a|
-            a.is_a? GBTiles::GBR::TileSet::Objects::Producer
+            a.object_type == GBTiles::GBR::TileSet::OBJECT_TYPE[:producer]
           }
         end
 
         def tile_data
           @objects.select{ |a|
-            a.is_a? GBTiles::GBR::TileSet::Objects::TileData
+            a.object_type == GBTiles::GBR::TileSet::OBJECT_TYPE[:tile_data]
           }
         end
 
         def tile_settings
           @objects.select{ |a|
-            a.is_a? GBTiles::GBR::TileSet::Objects::TileSettings
+            a.object_type == GBTiles::GBR::TileSet::OBJECT_TYPE[:tile_settings]
           }
         end
 
         def tile_export
           @objects.select{ |a|
-            a.is_a? GBTiles::GBR::TileSet::Objects::TileExport
+            a.object_type == GBTiles::GBR::TileSet::OBJECT_TYPE[:tile_export]
           }
         end
 
         def tile_import
           @objects.select{ |a|
-            a.is_a? GBTiles::GBR::TileSet::Objects::TileImport
+            a.object_type == GBTiles::GBR::TileSet::OBJECT_TYPE[:tile_import]
           }
         end
 
         def palettes
           @objects.select{ |a|
-            a.is_a? GBTiles::GBR::TileSet::Objects::Palettes
+            a.object_type == GBTiles::GBR::TileSet::OBJECT_TYPE[:palettes]
           }
         end
 
         def tile_pal
           @objects.select{ |a|
-            a.is_a? GBTiles::GBR::TileSet::Objects::TilePal
+            a.object_type == GBTiles::GBR::TileSet::OBJECT_TYPE[:tile_pal]
           }
         end
       end
